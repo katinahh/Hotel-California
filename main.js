ccfe23a5024063591fe8be53ac7f2d3ab0ce3db2
Kate Hrynchak <katinagg@gmail.com>
2020-12-15 21:45:15 +0200

    function + some object

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 "use strict"
 
-for (let i = 11; i < 23; i++){
-    var room = document.getElementById("room__acoommodation");
-    let src = '.jpg';
-       
-    room.innerHTML = room.innerHTML + `
-    <div class='room--block'>
-    <img class="img-of-room" src='images/rooms/${i}${src}' alt='images'>
-    <h1>Room ${i}</h1>
-    </div>`   
-    // room.innerHTML += `<img class="img-of-room" src='images/rooms/${i}${src}' alt='images'>`
+let room = document.getElementById("room__acoommodation");
+
+let renderRoomList = (itemsCount) =>{
+    let result = "";
+    for (let i = 1; i <= itemsCount; i++){
+        // let src = '.jpg';
+           
+        result = result + `
+        <div class='room--block'>
+        <img class="img-of-room" src='images/rooms/${i+10}.jpg' alt='images'>
+        <h1>Room ${i}</h1>
+        </div>`   
+        // room.innerHTML += `<img class="img-of-room" src='images/rooms/${i}${src}' alt='images'>`
+    }; 
+    room.innerHTML = result;
 };
 
+renderRoomList(10);
+// renderRoomList(20);
 
 
 
@@ -21,6 +28,45 @@ for (let i = 11; i < 23; i++){
 
 
 
+
+// for (let i = 11; i < 23; i++){
+//     let src = '.jpg';
+       
+//     room.innerHTML = room.innerHTML + `
+//     <div class='room--block'>
+//     <img class="img-of-room" src='images/rooms/${i}.jpg' alt='images'>
+//     <h1>Room ${i}</h1>
+//     </div>`   
+//     // room.innerHTML += `<img class="img-of-room" src='images/rooms/${i}${src}' alt='images'>`
+// };
+
+
+// let object__room = {
+//     image: 11,
+//     name: "Appartment",
+//     number: 1,
+//     price: 19.99
+// }
+
+// room.innerHTML = room.innerHTML + `
+//     <div class='room--block'>
+//     <img class="img-of-room" src='images/rooms/${object__room.image}.jpg' alt='images'>
+//     <h1>${object__room.name} ${object__room.number} <p>Price: ${object__room.price} $ </p>  </h1>
+//     </div>`   
+
+// let objectRoom2 = {
+//     image: 12,
+//     name: "Appartment",
+//     number: 2,
+//     price: 19.99
+// }
+// room.innerHTML = room.innerHTML + `
+//     <div class='room--block'>
+//     <img class="img-of-room" src='images/rooms/${objectRoom2.image}.jpg' alt='images'>
+//     <h1>${objectRoom2.name} ${objectRoom2.number} <p>Price: ${objectRoom2.price} $ </p>  </h1>
+//     </div>`   
+
+// let rooms = []
 
 
 
