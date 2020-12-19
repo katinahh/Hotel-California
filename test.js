e5646610edec6938ffaed8f139780b00da9aafe6
Kate Hrynchak <katinagg@gmail.com>
2020-12-19 22:06:12 +0200

    change bkg + logo

---
+++ test.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// let sheepCounted = 0;
+// while (sheepCounted < 10) {
+// console.log("I have counted" + sheepCounted + "sheep!");
+// sheepCounted++;
+// } console.log('zzzzzzzz');
+
+
+for(let sheep = 0; sheep < 5; sheep++){
+    console.log("Oh my " + sheep)
+}
+
+let someNum = 6;
+for(let num = 0; num <= someNum; num++){
+    console.log(num);
+}
+//-------------------------------ARR + FOR------------------
+// let animals = ["Lion", "Flamingo", "Polar Bear", "Giraffe"];
+// for (let any=0; any < animals.length; any++){
+//     console.log("This is zoo for " + animals[any] + " !")
+// }
